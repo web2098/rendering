c96116c531045fb582101396810018dd94c67ce3
eric-gressman <eric.gressman@hp.com>
2021-05-31 13:04:51 -0600

    Init checkin

---
+++ LiveRendering/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main( int argc, char** argv )
+{
+    std::puts("I have started");
+}
---
+++ Math/Vertex.h
@@ -0,0 +1,9 @@
+#pragma once
+
+
+class Vertex
+{
+    float x{0.0f};
+    float y{0.0f};
+    float z{0.0f};
+};
